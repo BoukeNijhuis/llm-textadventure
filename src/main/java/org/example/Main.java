db84c940b9053661560970997c9f263edf96127d
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2025-05-09 20:06:23 +0200

    introduced a more generic repeat counter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,9 +11,10 @@ import java.util.function.Function;
 
 public class Main {
 
-//    private static final Game game = new Zork();
-    private static final Game game = new VoiceAdventure();
-    private static int iDoNotUnderstandCounter = 0;
+    //    private static final Game game = new Zork();
+    private static final Game game = new Zork(); //new VoiceAdventure();
+    private static int repeatCounter = 0;
+    private static String repeatPhrase;
 
     public static void main(String[] args) throws IOException {
 
@@ -21,17 +22,15 @@ public class Main {
         game.start();
 
 //        OllamaChatModel model = OllamaChatModel.builder()
-//                .modelName("deepseek-r1:14b")
+//                .modelName("gemma3:12b")
 //                .baseUrl("http://localhost:11434")
 //                .build();
+
+        // todo: introduce two class with models: Ollama model & Gemini model (should contain rate limit logic)
 
         ChatLanguageModel model = VertexAiGeminiChatModel.builder()
                 .project("voiceadventure-3cf8a")
                 .location("us-central1")
-//                .modelName("gemini-exp-1206")
-//                .modelName("gemini-2.0-flash-exp")
-//                .modelName("gemini-2.0-flash-thinking-exp-01-21")
-//                .modelName("gemini-2.0-pro-exp-02-05")
                 .modelName("gemini-2.5-pro-exp-03-25")
                 // prevents rate limiter logging
                 .maxRetries(1)
@@ -60,8 +59,6 @@ public class Main {
         // game loop
         for (int i = 0; i < 100000; i++) {
 
-            modelInput = updateOutputWhenTheGameKeepsSayingIDoNotUnderstand(modelInput);
-
             // do not check the provided instructions
             String command = getCommand(chain, modelInput, true);
 
@@ -73,29 +70,42 @@ public class Main {
                 if (modelInput.contains(game.getCompletionString())) {
                     System.exit(0);
                 }
+
+                modelInput = updateOutputWhenTheGameKeepsRepeating(modelInput);
             }
         }
     }
 
-    private static String updateOutputWhenTheGameKeepsSayingIDoNotUnderstand(String output) {
-        if (output.startsWith("I don't understand that.")) {
-            if (++iDoNotUnderstandCounter == 5) {
-                output = "The game keeps repeating 'I don't understand that'. So walk around and check your inventory.";
+    private static String updateOutputWhenTheGameKeepsRepeating(String output) {
+        if (repeatPhrase != null && output.startsWith(repeatPhrase)) {
+            if (++repeatCounter == 5) {
+                output = "The game keeps repeating'. So walk around and check your inventory.";
                 // warn the spectator
-                System.out.println("\n\nThe counter tripped!");
+                System.out.println("\n\nThe counter tripped! With phrase: " + repeatPhrase);
 
-                // reset the counter
-                iDoNotUnderstandCounter = 0;
+                // reset the counter & phrase
+                resetCounterAndPhrase();
             }
 
         } else {
-            iDoNotUnderstandCounter = 0;
+            resetCounterAndPhrase();
         }
+        // update the repeat phrase
+        repeatPhrase = output;
         return output;
+    }
+
+    private static void resetCounterAndPhrase() {
+        repeatCounter = 0;
+        repeatPhrase = null;
     }
 
     private static String getCommand(ConversationalChain chain, String modelInput, boolean doChecks) {
         try {
+            if (modelInput.isEmpty()) {
+                modelInput = "Did not receive any input from the game.";
+            }
+
             String command = chain.execute(modelInput);
 
             // when using deepseek-r1 remove <think>*</think>
@@ -107,8 +117,7 @@ public class Main {
             if (doChecks) {
                 // the order matters
                 command = checkCommand(command, x -> x.split(" ").length > 6, "command too long", "Your answer is too long. Please give me EXACTLY ONE short command in the form of GO EAST, OPEN DOOR, etc.", chain);
-                command = checkCommand(command, x -> x.indexOf("\"") > 1, "quotes found", "Do not use quotes. Give only simple commands without any formatting.", chain);
-                command = checkCommand(command, x -> x.indexOf("'") > 1, "quotes found", "Do not use quotes. Give only simple commands without any formatting.", chain);
+                command = checkCommand(command, x -> x.indexOf("\"") > 1 || x.indexOf("'") > 1, "quotes found", "Do not use quotes. Give only simple commands without any formatting.", chain);
                 command = checkCommand(command, x -> x.startsWith("*"), "formatting found", "Do not use formatting like *. Give only simple commands without any formatting.", chain);
             }
             return command;
